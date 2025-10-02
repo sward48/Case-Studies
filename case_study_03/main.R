fef7fab98e3f36308f1dfbadc77c33d8773629b5
benedikt20 <benediktfadel@gmail.com>
2025-10-02 14:03:43 -0400

    added 2009 data

--- case_study_03/main.R
+++ case_study_03/main.R
@@ -2,6 +2,14 @@
 # Benedikt Farag, Elisa Sommer and Steve Ward
 # September 2025
 
+# Files not in the repo (from sharepoint): Both to go under rawdata/
+# 1) `Connecticut_CAMA_and_Parcel_Layer_3895368049124948111.csv`: property data 
+#    for CT from the link: 
+#    https://yaleedu-my.sharepoint.com/:u:/g/personal/brian_macdonald_yale_edu/EVAE5NykXsdLlqI4ufT6nBwBA0_oJh1jqjPioEMQ_G7Fmg?e=qoYauD
+# 2) Folder `Hamden_Sept2025`: Downloaded from the provided zip file: 
+#    https://yaleedu-my.sharepoint.com/:u:/g/personal/brian_macdonald_yale_edu/ERDOKwhS_s1Bn2f3GFAg5gABBrtaf-g-bAsOetpW44j7yA?e=QHhBRf
+
+
 print("Running main script...")
 
 system("quarto render scrape_html.qmd")
